1b42d2c8ce7c475e40fbb753fcbdcd927b62c908
Gonzalo Zaragoza <gzaragoza@gmail.com>
2022-01-09 19:39:59 +0100

    added whale project site

---
+++ sites/whale/xml/updateWhaleXML.php
@@ -0,0 +1,29 @@
+<?php 
+
+	//http://localhost:8888/whale-xml/updateWhaleXML.php?xvalue=777&yvalue=888&zvalue=999
+
+    $valorZ = $_GET['zvalue'];
+    $valorFecha = $_GET['fecha'];
+    $valorHora = $_GET['hora'];
+    $valorBateria = $_GET['bateria'];
+    $valorPanel = $_GET['panel'];
+        
+	// load the document
+	// the root node is <info/> so we load it into $info
+	//$whale = simplexml_load_file('http://espigoplatja.com/whale/xml/whale.xml');
+	$whale = simplexml_load_file('whale.xml');
+	
+	// update
+	$whale->valueZ = $valorZ;
+	$whale->fecha= $valorFecha;
+	$whale->hora= $valorHora;
+	$whale->bateria= $valorBateria;
+    $whale->panel= $valorPanel;
+		
+	// save the updated document
+	$whale->asXML('whale.xml');
+    
+    echo "done";
+    
+    
+?>
